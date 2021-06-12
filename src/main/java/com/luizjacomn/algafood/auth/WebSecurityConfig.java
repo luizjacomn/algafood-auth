ec6f06425f94d6b5772d4d64b4845ca48c6f86c0
Luiz Jacó <luizjacomn@gmail.com>
2021-06-12 11:19:38 -0300

    Adicionando clientes em memória

--- src/main/java/com/luizjacomn/algafood/auth/WebSecurityConfig.java
+++ src/main/java/com/luizjacomn/algafood/auth/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.luizjacomn.algafood.auth;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -25,5 +26,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 }
